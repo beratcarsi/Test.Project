6eab3fcbc66938d9a4cf60b799fa874f6eebe077
berat.carsi <beratcarsi@gmail.com>
2016-05-10 12:24:14 +0300

    observable, queue tests

---
+++ Test.Project/Tests/ObservableCollectionTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using Test.Project.TestModels;
+using NUnit.Framework;
+
+namespace Test.Project.Tests
+{
+    [TestFixture]
+    public class ObservableCollectionTest
+    {
+        [Test]
+        public void observable_collection_test()
+        {
+            var people = new ObservableCollection<Person>();
+
+            // event'i delege ediyoruz
+            people.CollectionChanged += CollectionChanged;
+
+            people.Add(new Person() { Name = "Kemal", SurName = "Sayar", Age = 23 });
+            people.Add(new Person() { Name = "Soner", SurName = "Okcu", Age = 19 });
+
+            people.RemoveAt(0);
+        }
+
+        private static void CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            Console.WriteLine("Action for this event: {0}", e.Action);
+
+            if (e.Action == NotifyCollectionChangedAction.Remove)
+            {
+                Console.WriteLine("Silinen insanlar:");
+                foreach (Person p in e.OldItems)
+                {
+                    Console.WriteLine(p.ToString());
+                }
+                Console.WriteLine();
+            }
+
+            if (e.Action == NotifyCollectionChangedAction.Add)
+            {
+                Console.WriteLine("Eklenen insanlar:");
+                foreach (Person p in e.NewItems)
+                {
+                    Console.WriteLine(p.ToString());
+                }
+            }
+        }
+
+    }
+}
---
+++ Test.Project/Tests/QueueListTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using Test.Project.TestModels;
+
+namespace Test.Project.Tests
+{
+    [TestFixture]
+    public class QueueListTest
+    {
+        [Test]
+        public void generic_queue_list_test()
+        {
+            //Queue<T>: FIFO; ilk giren ilk cikar mantigina gore calisir.
+            //Enqueue: Queue yapımıza yeni bir eleman eklemek için kullanılır.
+            //Dequeue: Queue yapımızdan sıradaki ilk elemanı alıp bu elemanı siler.
+            //Peek : Queue'deki nesneyi silmeden son elemanı döndürür.
+
+            Queue<Person> personQueue = new Queue<Person>();
+            personQueue.Enqueue(new Person() { Name = "Ahmet", SurName = "Kem", Age = 21 });
+            personQueue.Enqueue(new Person() { Name = "Mehmet", SurName = "Tac", Age = 15 });
+
+            Console.WriteLine($"Peeked : {personQueue.Peek()}");
+            Console.WriteLine($"Dequeue : {personQueue.Dequeue()}");
+
+            Console.WriteLine($"Peeked : {personQueue.Peek()}");
+            Console.WriteLine($"Dequeue : {personQueue.Dequeue()}");
+
+            try
+            {
+                Console.WriteLine($"Peeked : {personQueue.Peek()}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0}", e.Message);
+            }
+
+        }
+
+    }
+}
