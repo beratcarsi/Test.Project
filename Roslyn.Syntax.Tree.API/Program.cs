b3bca4eb9e2a3e6fc6e8117b4ca312bccafeb661
Berat Çarşı <beratcarsi@gmail.com>
2016-06-04 10:47:02 +0300

    roslyn syntax tree api

---
+++ Roslyn.Syntax.Tree.API/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp;
+
+
+namespace Roslyn.Syntax.Tree.API
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //var tree = CSharpSyntaxTree.ParseText(@"class myClass { public void myMethod(){ } }");
+        }
+
+    }
+}
---
+++ Test.Project/Tests/DatetimeTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Test.Project.Tests
+{
+    [TestFixture]
+    public class DatetimeTest
+    {
+        [Test]
+        public void datetime_test()
+        {
+            var datetime = DateTime.Now;
+            Console.WriteLine(datetime);
+            Console.WriteLine(datetime.DateTimeToString());
+        }
+
+    }
+
+    public static class DateTimeExtension
+    {
+        /// <summary>
+        /// Datetime to string. (e.g 16.05.2016 21:30)
+        /// </summary>
+        /// <param name="datetime"></param>
+        /// <returns>string 16.05.2016 21:30</returns>
+        public static string DateTimeToString(this DateTime datetime)
+        {
+            var result = datetime.ToString("dd.MM.yyyy HH:mm");
+            return result;
+        }
+    }
+}
